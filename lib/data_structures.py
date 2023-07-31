5de359946298e85ac45a4a5da8ae24bc88e02ec1
bouquet1 <bukuleta@gmail.com>
2023-07-31 10:59:48 -0700

    5 passed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,56 +16,45 @@ spicy_foods = [
     },
 ]
 
+
 # TASK: RETURNS A LIST OF STRINGS with the names of each spicy food
-
-
 def get_names(spicy_foods):
-    food_names = []   # Create an empty list to store the names
+    food_names = []
     for food in spicy_foods:
         food_name = food["name"]
-        food_names.append(food_name)  # Add the name to the list
+        food_names.append(food_name)
     return food_names
 
 
 names_of_spicy_food = get_names(spicy_foods)
 print(names_of_spicy_food)
-# output => ['Green Curry', 'Buffalo Wings', 'Mapo Tofu']
 
 
-# TASK: RETURNS A LIST OF DICTIONARIES where the heat level of the food is greater than 5.
+# TASK: RETURNS A LIST OF DICTIONARIES the heat level of the food is > than 5.
 def get_spiciest_foods(spicy_foods):
-    # list comprehension to filter out the foods' heat level > 5
     spiciest_food_list = [
         food for food in spicy_foods if food["heat_level"] > 5]
     return spiciest_food_list
 
 
 print(get_spiciest_foods(spicy_foods))
-# output => [{'name': 'Green Curry', 'cuisine': 'Thai', 'heat_level': 9}, {'name': 'Mapo Tofu', 'cuisine': 'Sichuan', 'heat_level': 6}]
 
 
 # TASK: OUTPUT TO THE TERMINAL each spicy food in the following format using
 # `print()`: `Buffalo Wings (American) | Heat Level: 🌶🌶🌶`.
 def print_spicy_foods(spicy_foods):
-    # for loop to iterate through each food.
-    # each iteration, the variable 'food' will hold the current dictionary that represents a spicy food with information name, cuisine, heat level.
     for food in spicy_foods:
         name = food["name"]
         cuisine = food["cuisine"]
         heat_level = food["heat_level"]
-        # Repeat the chili emoji based on the heat level
         heat_emoji = "🌶" * heat_level
         print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
 
 
 print(print_spicy_foods(spicy_foods))
-# output => Green Curry (Thai) | Heat Level: 🌶🌶🌶🌶🌶🌶🌶🌶🌶
-# Buffalo Wings (American) | Heat Level: 🌶🌶🌶
-# Mapo Tofu (Sichuan) | Heat Level: 🌶🌶🌶🌶🌶🌶
-# None (like undefined in JS becasue we didn't return a value we just printed a string)
 
 
-# TASK: function takes a list of spicy_foods and a string representing a cuisine, and returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
+# TASK: returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food["cuisine"] == cuisine:
@@ -73,13 +62,25 @@ def get_spicy_food_by_cuisine(spicy_foods, cuisine):
 
 
 print(get_spicy_food_by_cuisine(spicy_foods, "American"))
-# output => {'name': 'Buffalo Wings', 'cuisine': 'American', 'heat_level': 3}
 print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
-# output => {'name': 'Green Curry', 'cuisine': 'Thai', 'heat_level': 9}
 
 
+# TASK-5: OUTPUTS TO THE TERMINAL ONLY the spicy foods that have a heat level greater than 5, in the following format:
+# Buffalo Wings (American) | Heat Level: 🌶🌶🌶.
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            name = food["name"]
+            cuisine = food["cuisine"]
+            heat_level = food["heat_level"]
+            heat_emoji = "🌶" * heat_level
+            print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
+    # if food["heat_level"] > 9 denemesi yaptim
+    # else:
+    #     print("We don't have any food has heat_level more than 9")
+
+
+print(print_spiciest_foods(spicy_foods))
 
 
 def get_average_heat_level(spicy_foods):
