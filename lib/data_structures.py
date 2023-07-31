6315be21059bf75b2674e6c9686eeb5a7ef54150
bouquet1 <bukuleta@gmail.com>
2023-07-31 09:49:19 -0700

    2 passed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,6 +16,8 @@ spicy_foods = [
     },
 ]
 
+# RETURNS A LIST OF STRINGS with the names of each spicy food
+
 
 def get_names(spicy_foods):
     food_names = []   # Create an empty list to store the names
@@ -27,12 +29,18 @@ def get_names(spicy_foods):
 
 names_of_spicy_food = get_names(spicy_foods)
 print(names_of_spicy_food)
-
 # output => ['Green Curry', 'Buffalo Wings', 'Mapo Tofu']
 
 
+# RETURNS A LIST OF DICTIONARIES where the heat level of the food is greater than 5.
 def get_spiciest_foods(spicy_foods):
-    pass
+    # list comprehension to filter out the foods' heat level > 5
+    spiciest_food_list = [
+        food for food in spicy_foods if food["heat_level"] > 5]
+    return spiciest_food_list
+
+
+print(get_spiciest_foods(spicy_foods))
 
 
 def print_spicy_foods(spicy_foods):
