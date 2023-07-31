155dc7d6579be3e1c0c80198c7d65a73da2e8c87
bouquet1 <bukuleta@gmail.com>
2023-07-31 10:35:19 -0700

    4 passed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,7 +16,7 @@ spicy_foods = [
     },
 ]
 
-# RETURNS A LIST OF STRINGS with the names of each spicy food
+# TASK: RETURNS A LIST OF STRINGS with the names of each spicy food
 
 
 def get_names(spicy_foods):
@@ -32,7 +32,7 @@ print(names_of_spicy_food)
 # output => ['Green Curry', 'Buffalo Wings', 'Mapo Tofu']
 
 
-# RETURNS A LIST OF DICTIONARIES where the heat level of the food is greater than 5.
+# TASK: RETURNS A LIST OF DICTIONARIES where the heat level of the food is greater than 5.
 def get_spiciest_foods(spicy_foods):
     # list comprehension to filter out the foods' heat level > 5
     spiciest_food_list = [
@@ -41,14 +41,41 @@ def get_spiciest_foods(spicy_foods):
 
 
 print(get_spiciest_foods(spicy_foods))
+# output => [{'name': 'Green Curry', 'cuisine': 'Thai', 'heat_level': 9}, {'name': 'Mapo Tofu', 'cuisine': 'Sichuan', 'heat_level': 6}]
 
 
+# TASK: OUTPUT TO THE TERMINAL each spicy food in the following format using
+# `print()`: `Buffalo Wings (American) | Heat Level: 🌶🌶🌶`.
 def print_spicy_foods(spicy_foods):
-    pass
+    # for loop to iterate through each food.
+    # each iteration, the variable 'food' will hold the current dictionary that represents a spicy food with information name, cuisine, heat level.
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        # Repeat the chili emoji based on the heat level
+        heat_emoji = "🌶" * heat_level
+        print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
 
 
+print(print_spicy_foods(spicy_foods))
+# output => Green Curry (Thai) | Heat Level: 🌶🌶🌶🌶🌶🌶🌶🌶🌶
+# Buffalo Wings (American) | Heat Level: 🌶🌶🌶
+# Mapo Tofu (Sichuan) | Heat Level: 🌶🌶🌶🌶🌶🌶
+# None (like undefined in JS becasue we didn't return a value we just printed a string)
+
+
+# TASK: function takes a list of spicy_foods and a string representing a cuisine, and returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+
+print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+# output => {'name': 'Buffalo Wings', 'cuisine': 'American', 'heat_level': 3}
+print(get_spicy_food_by_cuisine(spicy_foods, "Thai"))
+# output => {'name': 'Green Curry', 'cuisine': 'Thai', 'heat_level': 9}
 
 
 def print_spiciest_foods(spicy_foods):
